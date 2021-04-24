56697999d03c4cc38a804ac7264211ff55818807
Shobhit Chourasia <>
2021-04-25 00:36:02 +0530

    1. Added observable protocol. 2. Added a new initial Home view controller programatically.

---
+++ MVVM_Template/Presentation Layer/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  MVVM_Template
+//
+//  Created by Shobhit on 25/04/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        title = "Home"
+        // Do any additional setup after loading the view.
+    }
+    
+}
